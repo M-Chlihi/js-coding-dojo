2e582c4aae4ae586e59879ea5155d6b013d74039
chlihi <chlihi101mohamed@gmail.com>
2025-10-01 09:35:06 +0200

    change some stuff

--- coding.js
+++ coding.js
@@ -1,14 +1,29 @@
-let age = prompt("What is your age?", 18);
 
-if (age < 18) {
-  function welcome() {
-    console.log("Hello DEAR YOUNG");
-  }
-} else {
-  function welcome() {
-    console.log("you are Greetings!");
-  }
+let m = ["Ahmed", "Osama", "Sayed","Stop", "Samera"];
+let colors = ["Amgad", "Samah", "Ameer", "Omar","Sabir", "Othman", "Amany", "Samia", "Anwar", "Omayma"];
+
+let count = 0 ;
+for (let i = 0; i < m.length; i++) {
+    if (m[i]==="Stop") {
+        break;
+    }
+     count++;
+    
 }
 
-// ...use it later
-welcome();
+document.write(`<h1>We have ${count} Admis</h1>`);
+
+for (let i = 0; i < count; i++) {
+    document.write(`<hr>`);
+    document.write(`<div class = "team">`);
+    document.write(`<span class = "admin-name"> the admine for team  ${i+1} is ${m[i]}</span>`);
+    document.write(`<h3> Team Membes: </h3>`);
+let k =1
+    for (let j = 0; j< colors.length; j++) {
+        if (m[i][0] === colors[j][0]) {
+        document.write(`<div class= "team"> ${k++} ${colors[j]}</div>`);
+        }
+    }
+document.write(`</div>`);
+
+}
