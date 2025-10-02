3cc3f8ae461ac7b96fbc11a11796f92fe19e0245
chlihi <chlihi101mohamed@gmail.com>
2025-10-02 21:45:19 +0200

    some change

--- coding.js
+++ coding.js
@@ -1,29 +1,40 @@
+
 // function showPrimes(n) {
-//   nextPrime: for (let i = 2; i < n; i++) {
+//   for (let i = 2; i < n; i++) {
+//     let isPrime = true;
 
 //     for (let j = 2; j < i; j++) {
-//       if (i % j == 0) continue nextPrime;
+//       if (i % j === 0) {
+//         isPrime = false;
+//         break; // stop checking, it's not prime
+//       }
 //     }
 
-//     console.log( i ); // a prime
+//     if (isPrime) {
+//       console.log(i);
+//     }
 //   }
 // }
-// showPrimes(4);
-function showPrimes(n) {
-  for (let i = 2; i < n; i++) {
-    let isPrime = true;
 
-    for (let j = 2; j < i; j++) {
-      if (i % j === 0) {
-        isPrime = false;
-        break; // stop checking, it's not prime
-      }
-    }
+// showPrimes(20);
 
-    if (isPrime) {
-      console.log(i);
-    }
+//SECOND
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return console.log('Did parents allow you?');
   }
 }
+function checkAge(age) {
+  
+    return(age > 18) ? true : console.log('Did mother allow you?');
+}
+function checkAge(age) {
+  
+    return(age > 18) || console.log('Did father allow you?');
+}
 
-showPrimes(20);
+checkAge(2);
+
+
