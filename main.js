0b28aad7dde01abb83dd8b03b09cc11bf94f6c0f
chlihi <chlihi101mohamed@gmail.com>
2025-10-15 08:11:19 +0200

    creat new

--- main.js
+++ main.js
@@ -1,1 +1,58 @@
-//DOM learning 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myIDele = document.getElementById("") ; //call the id of the element
+// let mytagele = document.getElementsByName("") ; //call the name of the element like p , h1...
+// let myclassname = document.myclassname("") ; //call the class  of the element like class= "myspane"
+// let myqueryele = document.querySelector(""); // anay ele and any thingg like #myspan , .class or p..
+// let myqueryeleall = document.querySelectorAll(""); // anay all  ele and any thingg like #myspan , .class or p..
+// //DOM manipulation
+// console.log(document.title);
+// console.log(document.form);
+// console.log(document.links[1].href);
+
+
+// let ele = document.querySelector(".js");
+
+// console.log(ele.innerHTML);
+// console.log(ele.textContent);
+// ele.innerHTML = "<p>mohamed chlihi is the best</p>";
+
+// let myLink = document.querySelector(".link");
+
+// console.log(myLink.getAttribute("href"));
+// console.log(myLink.getAttribute("class"));
+
+// myLink.setAttribute("href", "https://www.youtube.com/watch?v=XbmK22UjPz4")
+
+
