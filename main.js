6e48e8422a6579fc8f1d82b91fe5a931d29c2568
chlihi <chlihi101mohamed@gmail.com>
2025-10-23 07:37:39 +0200

    fixing new bugs

--- main.js
+++ main.js
@@ -1,58 +1,28 @@
 
+if (typeof document !== "undefined") {
+  const test = document.getElementById("headd"); 
+
+  if (!test) {
+    console.log('Element with id "headd" not found');
+  } else {
+    console.log('hasAttributes?', test.hasAttributes());       
+    console.log('has "id"?', test.hasAttribute('id'));         
+  }
+
+  
+  const h1s = document.getElementsByTagName("h1");
+  if (h1s.length > 0) {
+    console.log(h1s[0].attributes);
+
+    if (h1s[0].hasAttribute("class")) {
+      h1s[0].removeAttribute("class");
+    } else {
+      h1s[0].setAttribute("class", "newvalue");
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let myIDele = document.getElementById("") ; //call the id of the element
-// let mytagele = document.getElementsByName("") ; //call the name of the element like p , h1...
-// let myclassname = document.myclassname("") ; //call the class  of the element like class= "myspane"
-// let myqueryele = document.querySelector(""); // anay ele and any thingg like #myspan , .class or p..
-// let myqueryeleall = document.querySelectorAll(""); // anay all  ele and any thingg like #myspan , .class or p..
-// //DOM manipulation
-// console.log(document.title);
-// console.log(document.form);
-// console.log(document.links[1].href);
-
-
-// let ele = document.querySelector(".js");
-
-// console.log(ele.innerHTML);
-// console.log(ele.textContent);
-// ele.innerHTML = "<p>mohamed chlihi is the best</p>";
-
-// let myLink = document.querySelector(".link");
-
-// console.log(myLink.getAttribute("href"));
-// console.log(myLink.getAttribute("class"));
-
-// myLink.setAttribute("href", "https://www.youtube.com/watch?v=XbmK22UjPz4")
-
-
+    console.log(h1s[0].attributes);
+  }
+} else {
+  console.log("No DOM available (running in Node).");
+}
