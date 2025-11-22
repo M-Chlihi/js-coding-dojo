2492b77f10f3e09d3a29da494bb065fa14b60f66
M-Chlihi <chlihi101mohamed@gmail.com>
2025-11-22 17:57:25 +0100

    comments adding

---
+++ practice/main.js
@@ -0,0 +1,30 @@
+let myEle = document.createElement("div");
+let myAttr = document.createAttribute("data-change");
+myEle.className = "divclass";
+ myEle.setAttributeNode(myAttr);    
+ 
+ let text = document.createTextNode("this is a JS program");
+ let comment = document.createComment("this is new comment ");
+
+myEle.appendChild(text);
+myEle.appendChild(comment);
+document.body.appendChild(myEle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
