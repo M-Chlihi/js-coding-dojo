c92fee06172c29f54a98fcd4350b70ddf0526613
M-Chlihi <chlihi101mohamed@gmail.com>
2025-12-10 20:45:28 +0100

    setTimeout in the page

--- practice/main.js
+++ practice/main.js
@@ -1,10 +1,19 @@
-let mylink = document.getElementById("links")
-mylink.style.fontWeight = "bold";
-mylink.style.cssText = "color: red; font-Size: 20px; font-wight: bolder; text-decoration: double ";
+console.log(window.innerHeight);
+console.log("this is the width of the window: " + window.innerWidth);
 
-mylink.style.removeProperty("color");
-mylink.style.setProperty("color", "black");
+document.getElementById("newin").onclick = function () {
+  window.open();
+};
 
-// window.onload = function () {
-//   form1.focus();
-// };
+// let prmptMsg = prompt("are u sure ?", "wrtie yes or No");
+// console.log( typeof prmptMsg);
+
+let hadler = setTimeout(() => {
+    console.log("hi bro")
+}, 3000); 
+//excute the function after 3 seocnde 
+let btn = document.getElementById("win");
+btn.onclick = function () {
+  clearTimeout(hadler)
+}
+// if u click in the timeoput stop and the function cant execute 
