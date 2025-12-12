7499f87506e665189b220405ba466871581397b0
M-Chlihi <chlihi101mohamed@gmail.com>
2025-12-12 20:14:36 +0100

    display

--- practice/main.js
+++ practice/main.js
@@ -5,12 +5,15 @@ document.getElementById("newin").onclick = function () {
   window.open();
 };
 
-
 let hadler = setTimeout(() => {
-    console.log("hi bro")
-}, 3000); 
-//excute the function after 3 seocnde 
+  document.getElementById("newin").innerText = "click here";
+  console.log("you have clcked succesfuly")
+  document.getElementById("newin").style.display = "none";
+
+  
+}, 3000);
+//excute the function after 3 seocnde
 let btn = document.getElementById("win");
 btn.onclick = function () {
-  clearTimeout(hadler)
-}
+  clearTimeout(hadler);
+};
