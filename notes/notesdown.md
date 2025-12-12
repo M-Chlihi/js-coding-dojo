28300d3f313e712f4fd23b19ea6ac3019bc7b76a
M-Chlihi <chlihi101mohamed@gmail.com>
2025-12-12 21:09:10 +0100

    features of open scripting window config

--- notes/notesdown.md
+++ notes/notesdown.md
@@ -110,3 +110,28 @@ mylink.style.removeProperty("color");
 mylink.style.setProperty("color", "black");
 
 ```
+# BOM
+### setInterval & clearInterval
+```JS
+//p^ractise basics
+let cownt = document.querySelector("h2");
+let stopconter = document.getElementById("win");
+let parag = document.getElementById("text");
+        parag.innerText = ` thinking...` ;
+
+function decriment() {
+  cownt.innerHTML -= 1;
+
+  stopconter.onclick = function () {
+    clearInterval(counter);
+    parag.innerText = ` You stopped conter at ${cownt.innerHTML} ` ;
+  };
+
+  if (cownt.innerHTML === "0") {
+    clearInterval(counter);
+        parag.innerText = ` Tinming is Done. ` ;
+  }
+}
+let counter = setInterval(decriment, 1500);
+
+```
--- practice/main.js
+++ practice/main.js
@@ -1,19 +1,19 @@
-console.log(window.innerHeight);
-console.log("this is the width of the window: " + window.innerWidth);
+let cownt = document.querySelector("h2");
+let stopconter = document.getElementById("win");
+let parag = document.getElementById("text");
+        parag.innerText = ` thinking...` ;
 
-document.getElementById("newin").onclick = function () {
-  window.open();
-};
+function decriment() {
+  cownt.innerHTML -= 1;
 
-let hadler = setTimeout(() => {
-  document.getElementById("newin").innerText = "click here";
-  console.log("you have clcked succesfuly")
-  document.getElementById("newin").style.display = "none";
+  stopconter.onclick = function () {
+    clearInterval(counter);
+    parag.innerText = ` You stopped conter at ${cownt.innerHTML} ` ;
+  };
 
-  
-}, 3000);
-//excute the function after 3 seocnde
-let btn = document.getElementById("win");
-btn.onclick = function () {
-  clearTimeout(hadler);
-};
+  if (cownt.innerHTML === "0") {
+    clearInterval(counter);
+        parag.innerText = ` Tinming is Done. ` ;
+  }
+}
+let counter = setInterval(decriment, 1500);
