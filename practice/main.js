730841e2d1024901fbff86618a2d769383861726
M-Chlihi <chlihi101mohamed@gmail.com>
2025-12-21 21:43:01 +0100

    removeschlid

--- practice/main.js
+++ practice/main.js
@@ -6,22 +6,45 @@ let form = document.querySelector("form");
 window.onload = function () {
   input.focus();
 };
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-const taskText = input.value.trim();
+  const taskText = input.value.trim();
+
   if (taskText === "") return;
-  const li = document.createElement("li");
+
+  const li = document.createElement("div");
   li.textContent = taskText;
-  // li.style.width = "100%"
-  li.style.border = "black 2px solid"
-
-
+  li.style.border = "black 2px solid";
   taskadd.appendChild(li);
-  input.value = ""; 
+
+  const delet = document.createElement("button");
+  delet.textContent = "delete";
+  li.appendChild(delet);
+  delet.style.padding = "6px 11px";
+  delet.style.fontSize = "13px";
+  delet.style.fontWeight = "600";
+  delet.style.border = "2px brown solid";
+  delet.style.background = "red";
+  delet.style.borderRadius = "4px";
+  delet.style.color = "#fffdfd";
+  delet.style.cursor = "pointer";
+  
+  input.value = "";
+  
+  delet.addEventListener("click", function () {
+  li.parentNode.removeChild(li);
+    });
 });
 
+// Source - https://stackoverflow.com/q/5933157
+// Posted by Newbie Coder, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-21, License - CC BY-SA 3.0
 
-
+// function removeDummy() {
+//  var elem = document.getElementById('dummy');
+//  elem.parentNode.removeChild(elem);
+// }
 
 
 
