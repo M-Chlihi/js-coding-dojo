cd7870fdc9225d1844c1530d8b5563e204c411f0
M-Chlihi <chlihi101mohamed@gmail.com>
2025-12-24 22:29:33 +0100

    newfile of pactice

---
+++ practice/main.js
@@ -0,0 +1,7 @@
+
+let friends = ["mohamed", "adam", "yakin", ["hamza","abdesalam", ["ferdouas", "tasnim"]]]
+
+let a = friends[3][0]
+let b = friends[3][2][1]
+console.log(a)//hamza
+console.log(b)//tasnim
