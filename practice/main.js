65f84d84eb36a2737844f4dd1d9383bf2e20975f
M-Chlihi <chlihi101mohamed@gmail.com>
2025-12-24 22:49:15 +0100

    destructuring arrays

--- practice/main.js
+++ practice/main.js
@@ -1,7 +1,24 @@
+let user = {
+  name: "chlihi",
+  age: 19,
+  firstName: "mohamed",
+  contry: "Morocco",
+};
 
-let friends = ["mohamed", "adam", "yakin", ["hamza","abdesalam", ["ferdouas", "tasnim"]]]
+let myNm = user.name;
+let myage = user.age;
+let myfrst = user.firstName;
+let mycntr = user.contry;
+console.log(myNm);
+console.log(myage);
+({ name: myNm,age: myage, firstName: myfrst, contry: mycntr } = user);
+//reriting the elemnt in the object after decalring
+console.log(mycntr);
+console.log(myage);
 
-let a = friends[3][0]
-let b = friends[3][2][1]
-console.log(a)//hamza
-console.log(b)//tasnim
+// let friends = ["mohamed", "adam", "yakin", ["hamza","abdesalam", ["ferdouas", "tasnim"]]]
+
+// let a = friends[3][0]
+// let b = friends[3][2][1]
+// console.log(a)//hamza
+// console.log(b)//tasnim
