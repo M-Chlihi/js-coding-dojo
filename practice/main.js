8fab6cb68a4e7fc71f90ab76c86869191a164c5e
M-Chlihi <chlihi101mohamed@gmail.com>
2025-12-25 22:10:03 +0100

    nested ar & ob challenge

--- practice/main.js
+++ practice/main.js
@@ -20,35 +20,4 @@ function showdetails({name: n, age:ag, skills:{javascript: js} } =user ) {
   ur age is: ${ag} 
   ur skills are: ${js}`;
 }
-
-// const {
-//   name: nm,
-//   age: g,
-//   firstName: fstnm,
-//   contry: cnt,
-//   skills: { html: ht, javascript: js },
-// } = user;
-// console.log(nm);
-// console.log(g);
-// console.log(fstnm);
-// console.log(cnt);
-// console.log(js);
-// console.log(ht);
-
-// let myNm = user.name;
-// let myage = user.age;
-// let myfrst = user.firstName;
-// let mycntr = user.contry;
-// console.log(myNm);
-// console.log(myage);
-// ({ name: myNm,age: myage, firstName: myfrst, contry: mycntr } = user);
-// //reriting the elemnt in the object after decalring
-// console.log(mycntr);
-// console.log(myage);
-
-// let friends = ["mohamed", "adam", "yakin", ["hamza","abdesalam", ["ferdouas", "tasnim"]]]
-
-// let a = friends[3][0]
-// let b = friends[3][2][1]
-// console.log(a)//hamza
-// console.log(b)//tasnim
+//challenge 
