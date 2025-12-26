8c4bbbf860048ed9fea1b9b145b9087cb738e77a
M-Chlihi <chlihi101mohamed@gmail.com>
2025-12-26 20:19:21 +0100

    distructuring boject advence challenge

--- practice/main.js
+++ practice/main.js
@@ -1,23 +1,39 @@
-let user = {
-  name: "chlihi",
-  age: 19,
-  firstName: "mohamed",
-  contry: "Morocco",
-  skills: {
-    html: 70,
-    javascript: 80,
-  },
-};
-//creating elelment
-const p = document.createElement("h2");
-const body = document.querySelector("body");
-showdetails(user);
-body.appendChild(p);
-//function for testing destructions fct
-function showdetails({name: n, age:ag, skills:{javascript: js} } =user ) {
 
-  p.innerText = `ur last name is: ${n} 
-  ur age is: ${ag} 
-  ur skills are: ${js}`;
-}
-//challenge 
+let chosen = 3;
+let myFrnds = [ 
+  {title: "moahmed", age: 39, availiable: true, skills:["html", "css"] },
+  {title: "adam", age: 16, availiable: false, skills:["python", "tyescript"] },
+  {title: "hamza", age: 8, availiable: true, skills:["php", "GO"] },
+]
+
+
+let {title,age , availiable, skills} = myFrnds[chosen - 1];
+let lastskills = skills[skills.length -1]
+console.log(`Ur Name: ${title}`)
+console.log(`Ur Age: ${age}`)
+console.log(`status: ${availiable ? "availibale": "Not availiabele"}`)
+console.log(`Ur skill is : ${lastskills}`)
+
+// let user = {
+//   name: "chlihi",
+//   age: 19,
+//   firstName: "mohamed",
+//   contry: "Morocco",
+//   skills: {
+//     html: 70,
+//     javascript: 80,
+//   },
+// };
+// //creating elelment
+// const p = document.createElement("h2");
+// const body = document.querySelector("body");
+// showdetails(user);
+// body.appendChild(p);
+// //function for testing destructions fct
+// function showdetails({name: n, age:ag, skills:{javascript: js} } =user ) {
+
+//   p.innerText = `ur last name is: ${n} 
+//   ur age is: ${ag} 
+//   ur skills are: ${js}`;
+// }
+// //challenge 
