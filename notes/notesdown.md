fca6623128651556cbf303da562c40b989e63225
M-Chlihi <chlihi101mohamed@gmail.com>
2025-12-31 14:07:53 +0100

    complixity checking every()

--- notes/notesdown.md
+++ notes/notesdown.md
@@ -334,3 +334,22 @@ mainArr.copyWithin(0, -2)
 
 console.log(mainArr)
   ```
+
+  ##  every() method checks if all the array elements pass the given test function.
+  ```JS
+  // function that checks whether
+// the age is 18 or above
+function checkAdult(age) {
+  console.log("testing  ");// will repeat it 5 times 
+  return age >= 18;
+}
+
+const ageArray = [34, 23, 20, 26, 18];
+
+//checks if all the array elements
+// pass the checkAdult() function
+let check = ageArray.every(checkAdult);
+console.log(check);
+// Output: false
+
+  ```
--- practice/main.js
+++ practice/main.js
@@ -1,12 +1,14 @@
-let mainArr = [1, 2,12, 3, 4, 5, 6, "chlihi"];
-let rang = {
-    min: 10,
-    max: 20,
+// function that checks whether
+// the age is 18 or above
+function checkAdult(age) {
+  console.log("testing  ");// will repeat it 5 times 
+  return age >= 18;
 }
 
-let check = mainArr.some(function (e) {
-    console.log(this.max);
-    console.log(this.min);
-  return e >= this.min && e <= this.max ;
-}, rang);
-console.log(check)
+const ageArray = [34, 23, 20, 26, 18];
+
+//checks if all the array elements
+// pass the checkAdult() function
+let check = ageArray.every(checkAdult);
+console.log(check);
+// Output: false
